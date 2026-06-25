dbff0f42a27e51122b5a653105254e2ed4228351
Lord Geraldkachi <samson.olawoye@gmail.com>
2026-06-25 10:56:39 +0100

    update on he protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,9 @@
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { isAuthenticated } = useAuth();
-  if (!isAuthenticated) return <Navigate to="/login" replace/>;
+  const {
+    //  isAuthenticated
+     } = useAuth();
+  // if (!isAuthenticated) return <Navigate to="/login" replace/>;
   return <>{children}</>;
 }
